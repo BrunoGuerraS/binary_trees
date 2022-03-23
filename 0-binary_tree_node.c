52e284febb5066074522e21d3cd9013914d2c377
mr.b <bruno932151@gmail.com>
2022-03-23 11:20:56 -0500

    0-binary_tree_node.c line 12 and 24 was reamove

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,11 +9,9 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node = NULL;
 
-	
 	node = malloc(sizeof(binary_tree_t));
 	if (!node)
 		return (NULL);
-
 
 	node->n = value;
 	node->parent = parent;
@@ -21,6 +19,5 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	node->right = NULL;
 
 	return (node);
-	
 
 }
