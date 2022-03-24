33541ae3c13d2178b58941dcb2fc2b151fdc41c1
mr.b <bruno932151@gmail.com>
2022-03-24 16:52:38 -0500

    variables dright and dleft was added

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,15 +6,18 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t dright = 0, dleft = 0;
+
 	if (!tree)
 		return (0);
-	if (!tree->left && !tree->right)
-		return (0);
 	if (tree->left)
-		return (1 + binary_tree_height(tree->left));
+		dleft =  (1 + binary_tree_height(tree->left));
+	if (tree->right)
+		dright = (1 + binary_tree_height(tree->right));
+	if (dright > dleft)
+		return (dright);
 	else
-		return (1 + binary_tree_height(tree->right));
-
-	return (0);
+		return (dleft);
+	
 }
 
